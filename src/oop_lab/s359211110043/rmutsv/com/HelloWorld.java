13bccd594e2e403d12b7bc9cca71e499da942edb
aornuma <aornuma3003@gmail.com>
2017-11-19 09:49:01 +0700

    Testing upload to Github

---
+++ src/oop_lab/s359211110043/rmutsv/com/HelloWorld.java
@@ -0,0 +1,13 @@
+package oop_lab.s359211110043.rmutsv.com;
+
+public class HelloWorld
+{
+//    create main_method
+public static void main(String[] args) {
+
+    System.out.println("Hello world, I'm AORN");
+    System.out.println("My name is Aornuma Datesri");
+
+}//main
+
+}//class

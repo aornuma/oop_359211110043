1d6540d4457c001b1e119539db460ff83365eda5
aornuma <aornuma3003@gmail.com>
2017-11-19 10:32:36 +0700

    Testing upload to Github

--- src/oop_lab/s359211110043/rmutsv/com/HelloWorld.java
+++ src/oop_lab/s359211110043/rmutsv/com/HelloWorld.java
@@ -7,7 +7,9 @@ public static void main(String[] args) {
 
     System.out.println("Hello world, I'm AORN");
     System.out.println("My name is Aornuma Datesri");
-
+//    Add information
+    System.out.println("Faculty : Management Technology");
+    System.out.println("RMUTSV");
 }//main
 
 }//class

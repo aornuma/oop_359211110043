0ef6cb2a4cdb90e3f0608ad03cbc9ca9f3e366cd
aornuma <aornuma3003@gmail.com>
2017-11-20 16:02:35 +0700

    Variables and Data Types

--- src/ooplab2/TestInputData.java
+++ src/ooplab2/TestInputData.java
@@ -1,10 +1,27 @@
 package ooplab2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.management.BufferPoolMXBean;
+import java.util.Scanner;
+
 public class TestInputData {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("What is your name? :");
+        String name = reader.readLine();
+        System.out.println("Your name is"+name);
+        System.out.print("How old are you?: ");
+        int age = Integer.parseInt(reader.readLine());
+        System.out.println("Your are"+age+"years old.");
 
-
+        //Scanner
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Whare are you?: ");
+        String address = scanner.nextLine();
+        System.out.println("You from: "+address);
 
     }//main
 
